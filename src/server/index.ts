74ccbe7f252e7e781cb7c67be9c9815a552d821d
yuliasemenyuk <semenyukyp@gmail.com>
2025-07-18 14:46:09 +0300

    routes

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
-
-const PORT = process.env.PORT || 3001;
+import { router as checkoutRouter } from "./routes/wayforpay/checkout";
+import { router as webhookRouter } from "./routes/wayforpay/webhook";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/payment/wayforpay/checkout", checkoutRouter);
+app.use("/payment/wayforpay/webhook", webhookRouter);
 
 export { app };
